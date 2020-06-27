5ac13968f460fde75c4f99874b436ba6687367fa
briantran98 <98brian.tran@gmail.com>
2020-06-27 02:48:15 +0000

    Removed template sql and added sql for tables

---
+++ db/schema/01_schema.sql
@@ -0,0 +1,40 @@
+DROP TABLE IF EXISTS users CASCADE;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    phone SMALLINT,
+    date_of_birth DATE,
+    city VARCHAR(255),
+    country VARCHAR(255),
+    province VARCHAR(255),
+    password VARCHAR(255)
+);
+
+CREATE TABLE products (
+    id SERIAL PRIMARY KEY NOT NULL,
+    user_id INTEGER REFERENCES users(id) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    price BIGINT NOT NULL,
+    description TEXT NOT NULL,
+    quantity BIGINT NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    active BOOLEAN,
+    featured BOOLEAN,
+    cover_photo_url VARCHAR(255),
+    product_photo_url VARCHAR(255)
+);
+
+CREATE TABLE favourites (
+    id SERIAL PRIMARY KEY NOT NULL,
+    product_id INTEGER REFERENCES products(id) NOT NULL,
+    active BOOLEAN
+);
+
+CREATE TABLE messages (
+    id SERIAL PRIMARY KEY NOT NULL,
+    user_id INTEGER REFERENCES users(id),
+    body TEXT,
+    created_at DATE DEFAULT NOW(),
+    product_id INTEGER REFERENCES products(id)
+);
