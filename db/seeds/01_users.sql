863f9abfce6517867dcd69ec111938344f5cabd1
Eric Ho <endonoho@gmail.com>
2020-06-27 15:18:21 -0700

    Delete schema and seed data

--- db/seeds/01_users.sql
+++
@@ -1,6 +0,0 @@
--- Users table seeds here (Example)
-INSERT INTO users (name) VALUES ('Alice');
-INSERT INTO users (name) VALUES ('Kira');
-INSERT INTO users (name) VALUES ('John');
-INSERT INTO users (name) VALUES ('Kate');
-INSERT INTO users (name) VALUES ('Jacky');
