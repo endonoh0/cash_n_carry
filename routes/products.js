a67344074cc1b70e809e01c964c4999fa38dfd53
briantran98 <98brian.tran@gmail.com>
2020-06-27 20:26:12 +0000

    fixed products post

--- routes/products.js
+++ routes/products.js
@@ -19,10 +19,10 @@ module.exports = (router, db) => {
 
   router.post('/', (req, res) => {
     const userId = req.session.userId;
-    const post = {...req.body, seller_id: userId, active: true, featured: true};
-    db.query(`INSERT INTO products (title, description, price, quantity, location, cover_photo_url, product_photo_url)
+    const {title, description, price, quantity, location, cover_photo_url, product_photo_url, seller_id, active, featured} = {...req.body, seller_id: userId, active: true, featured: true};
+    db.query(`INSERT INTO products (title, description, price, quantity, location, cover_photo_url, product_photo_url, seller_id, active, featured)
     VALUES ($1, $2, $3, $4, $5, $6, $7)
-    RETURNING *;`, [post]
+    RETURNING *;`, [title, description, price, quantity, location, cover_photo_url, product_photo_url, seller_id, active, featured]
     );
   });
 
