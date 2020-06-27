a0fde457eb6d1fcc5969c71ad42e2de2c1af54fe
briantran98 <98brian.tran@gmail.com>
2020-06-27 20:11:25 +0000

    Adding query insert into the db for post products/

--- routes/products.js
+++ routes/products.js
@@ -15,7 +15,11 @@ module.exports = (router, db) => {
 
   router.post('/', (req, res) => {
     const userId = req.session.userId;
-
+    const post = {...req.body, seller_id: userId, active: true, featured: true};
+    db.query(`INSERT INTO products (title, description, price, quantity, location, cover_photo_url, product_photo_url)
+    VALUES ($1, $2, $3, $4, $5, $6, $7)
+    RETURNING *;`, [post]
+    );
   });
 
   return router;
