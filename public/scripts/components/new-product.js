fd3b24614d0b2bd522f0c5a0a88dec92e607eb73
Eric Ho <endonoho@gmail.com>
2020-07-02 11:04:06 -0700

    Update date format in product show page

--- public/scripts/components/new-product.js
+++ public/scripts/components/new-product.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
-    // Fetch products
+  /**
+   * Fetch all products
+   */
     $.ajax({
         method: 'GET',
         url: '/api/products',
     }).done((data) => {
+      console.log(typeof data, data);
         renderProducts(data.data);
     });
 });
@@ -38,13 +41,11 @@ const createProductElement = function (product) {
                     <button id="reply" class="reply">Reply</button>
                     <button><i class="fa fa-heart"></i></button>
                 </div>
-                <span>Posted 10 days ago...</span>
+                <span>Posted ${moment(product.created_at).format("ddd @ h:mmA")}</span>
               </div>
             <h3>${product.title} (${product.location})</h3>
             <img class="w-full" src="${product.product_photo_url}" alt="">
-            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard
-            dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen
-            book.</p>
+            <p>${product.description}</p>
         </div>
     </div>
 `);
